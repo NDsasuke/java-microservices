9b1568449b06cf053d7711b367eceeb06a6b1bf5
jolly6 <98voonfusing@gmail.com>
2022-12-26 17:42:25 +0800

    Service discovery with Eureka [ Spring version: 3.0.1 ]

--- inventory-service/src/main/java/com/jolly/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/jolly/inventoryservice/InventoryServiceApplication.java
@@ -4,11 +4,9 @@ import com.jolly.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- inventory-service/src/main/java/com/jolly/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/jolly/inventoryservice/model/Inventory.java
@@ -1,6 +1,6 @@
 package com.jolly.inventoryservice.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
